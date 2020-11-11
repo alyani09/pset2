3625233cec64f15f0096fb44f0ade792e5872ee9
alyani09 <73565807+alyani09@users.noreply.github.com>
2020-11-11 22:39:04 +0800

    Add files via upload

---
+++ pset2/registerr.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2>Lab Test 2</h2>
+    <?php
+    echo "Welcome to this page, this is your email :";
+    echo $_POST["email"];
+    ?>
+</body>
+</html>
